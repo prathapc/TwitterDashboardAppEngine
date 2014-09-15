a59a5892cff9947ee500c3d0ddccb96258663ee5
prathapc <prathap.choudhury@gmail.com>
2014-09-16 00:26:34 +0530

    changing signin servlet request token

--- target/TwitterDashboard-1.0-SNAPSHOT/js/twitterLogin.js
+++ target/TwitterDashboard-1.0-SNAPSHOT/js/twitterLogin.js
@@ -2,6 +2,6 @@ var width = 800;
 var height= 550;
 var left = (screen.width/2)-(width/2);
 function twitter_login() {
-	window.open("http://127.0.0.1:8085/TwitterSignin/signin",
+	window.open("https://fresh-fusion-704.appspot.com/signin",
 			"mywindow","menubar=0,resizable=0,scrollbars=no,width="+width+",height="+height+",status=0,toolbar=0,left="+left+",top=120");
 }
