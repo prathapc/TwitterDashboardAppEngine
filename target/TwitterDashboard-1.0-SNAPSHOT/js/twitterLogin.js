91fafdf3477704adb5a5d2fe4dcc83e22e042ca7
prathapc <prathap.choudhury@gmail.com>
2014-09-16 09:47:01 +0530

    debugging after rollback using appcfg-compile error fix3

--- target/TwitterDashboard-1.0-SNAPSHOT/js/twitterLogin.js
+++ target/TwitterDashboard-1.0-SNAPSHOT/js/twitterLogin.js
@@ -2,6 +2,6 @@ var width = 800;
 var height= 550;
 var left = (screen.width/2)-(width/2);
 function twitter_login() {
-	window.open("https://fresh-fusion-704.appspot.com/signin",
+	window.open("https://fresh-fusion-704.appspot.com/TwitterDashboard/signin",
 			"mywindow","menubar=0,resizable=0,scrollbars=no,width="+width+",height="+height+",status=0,toolbar=0,left="+left+",top=120");
 }
