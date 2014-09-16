477fb606543e93d225ba18169e45cda66eb0fd0a
prathapc <prathap.choudhury@gmail.com>
2014-09-16 12:06:39 +0530

    changing way of setting consumer key in servlet

--- src/main/java/com/akosha/servlet/TwitterSigninServlet.java
+++ src/main/java/com/akosha/servlet/TwitterSigninServlet.java
@@ -1,7 +1,6 @@
 package com.akosha.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -29,21 +28,25 @@ public class TwitterSigninServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-PrintWriter out = response.getWriter();
-out.println ("<html><body><script>alert('inside sigin servlet');</script></body></html>");
 		try {
-			System.out.println("Inside servlet");
-			Twitter twitter = new TwitterFactory().getInstance();
+			//System.out.println("Inside servlet");
+			//Twitter twitter = new TwitterFactory().getInstance();
+ConfigurationBuilder cb = new ConfigurationBuilder();
+		    cb.setDebugEnabled(true)
+		          .setOAuthConsumerKey("4SkAK5YQAuajrJjyNdhp2uhku")
+		          .setOAuthConsumerSecret("fs17RLRIJEP99Trv3PehzxsN0Yg1TyMu5Jg8xVoy0ptju14MZo");
+		    
+	        Twitter twitter = new TwitterFactory(cb.build()).getInstance();
+	        
 			RequestToken requestToken;
 			request.getSession().setAttribute("twitter", twitter);
 			
 			//setting consumer key and consumer secret
-			twitter.setOAuthConsumer("4SkAK5YQAuajrJjyNdhp2uhku", "fs17RLRIJEP99Trv3PehzxsN0Yg1TyMu5Jg8xVoy0ptju14MZo");			
+			//twitter.setOAuthConsumer("4SkAK5YQAuajrJjyNdhp2uhku", "fs17RLRIJEP99Trv3PehzxsN0Yg1TyMu5Jg8xVoy0ptju14MZo");			
 			requestToken = twitter.getOAuthRequestToken("https://fresh-fusion-704.appspot.com/views/redirect.jsp");			
 			String authURL = requestToken.getAuthenticationURL();
 			System.out.println("authURL: "+authURL);
 			request.getSession().setAttribute("requestToken", requestToken);
-out.println ("<html><body><script>alert('goint to sendredirect from signin!');</script></body></html>");
 			response.sendRedirect(authURL);
 			
 			//request.getRequestDispatcher("TwitterDashboard.jsp").forward(request, response);
