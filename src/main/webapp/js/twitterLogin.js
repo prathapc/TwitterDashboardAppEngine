82217c7752eef63ff7c8034d9edf988d006d36f3
prathapc <prathap.choudhury@gmail.com>
2014-09-16 20:24:25 +0530

    opening signin in same window

--- src/main/webapp/js/twitterLogin.js
+++ src/main/webapp/js/twitterLogin.js
@@ -3,5 +3,5 @@ var height= 550;
 var left = (screen.width/2)-(width/2);
 function twitter_login() {
 	window.open("https://fresh-fusion-704.appspot.com/signin/",
-			"mywindow","menubar=0,resizable=0,scrollbars=no,width="+width+",height="+height+",status=0,toolbar=0,left="+left+",top=120");
+			"_self","menubar=0,resizable=0,scrollbars=no,width="+width+",height="+height+",status=0,toolbar=0,left="+left+",top=120");
 }
